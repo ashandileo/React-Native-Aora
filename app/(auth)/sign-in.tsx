f2f15633d3551702c88b7902936a0f57f2e275c1
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-05-16 09:18:42 +0700

    feat: create sign-in page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "./../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-white text-2xl text-semibold font-psemibold mt-10">
+            Log in to Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
