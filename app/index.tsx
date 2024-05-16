7fd15a4b0b939356a245717488f969bf1efabcb1
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-05-16 18:06:13 +0700

    feat: handle users context

--- app/index.tsx
+++ app/index.tsx
@@ -6,8 +6,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "./../constants";
 import CustomButton from "@/components/CustomButton";
+import { useGlobalContext } from "@/context/GlobalProvider";
 
 const RootLayout = () => {
+  const { isLoading, isLoggedIn } = useGlobalContext() as any; // TODO
+
+  if (!isLoading && isLoggedIn) return <Redirect href={"/home"} />;
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
---
+++ context/GlobalProvider.tsx
@@ -0,0 +1,51 @@
+import { getCurrentUser } from "@/lib/appwrite";
+import { createContext, useContext, useState, useEffect } from "react";
+
+const GlobalContext = createContext({});
+
+export const useGlobalContext = () => {
+  return useContext(GlobalContext);
+};
+
+interface IGlobalProvider {
+  children: React.ReactNode;
+}
+
+export const GlobalProvider = ({ children }: IGlobalProvider) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getCurrentUser()
+      .then((res) => {
+        if (res) {
+          setIsLoggedIn(true);
+          setUser(res);
+        } else {
+          setIsLoggedIn(false);
+          setUser(null);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        user,
+        setUser,
+        isLoading,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
