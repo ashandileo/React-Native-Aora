1e572d66add814c9fcaafb64899be530d92fea8d
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-05-18 21:04:13 +0700

    fix: remove unused parenthesis

--- app/index.tsx
+++ app/index.tsx
@@ -11,7 +11,7 @@ import { useGlobalContext } from "@/context/GlobalProvider";
 const RootLayout = () => {
   const { isLoading, isLoggedIn } = useGlobalContext() as any; // TODO
 
-  if (!isLoading && isLoggedIn) return <Redirect href={"/home"} />;
+  if (!isLoading && isLoggedIn) return <Redirect href="/home" />;
 
   return (
     <SafeAreaView className="bg-primary h-full">
