04f7baedaeb24b09fe99a80d428e48f6b1994652
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-05-20 19:11:12 +0700

    feat: add empty-state and trending component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -4,11 +4,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../../constants";
 import SearchInput from "@/components/SearchInput";
+import Trending from "@/components/Trending";
+import EmptyState from "@/components/EmptyState";
 
 const Home = () => {
   const data = [{ id: 1 }, { id: 2 }, { id: 3 }];
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
         data={data}
         // @ts-ignore
@@ -43,8 +45,16 @@ const Home = () => {
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
+
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }]} />
             </View>
           </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos created yet"
+          />
         )}
       />
     </SafeAreaView>
